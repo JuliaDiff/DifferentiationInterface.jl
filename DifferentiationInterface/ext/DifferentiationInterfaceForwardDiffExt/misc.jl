f8c3b42690cdb585da62a886713a78f9fd1054a1
Qingyu Qu <2283984853@qq.com>
2025-01-07 13:22:16 +0800

    Add overloaded_inputs for pushforwardprep

--- DifferentiationInterface/ext/DifferentiationInterfaceForwardDiffExt/misc.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceForwardDiffExt/misc.jl
@@ -1,3 +1,2 @@
-DI.overloaded_inputs(prep::ForwardDiffTwoArgPushforwardPrep) = prep.xdual_tmp
 DI.overloaded_inputs(prep::ForwardDiffOneArgJacobianPrep) = prep.config.duals[2]
 DI.overloaded_inputs(prep::ForwardDiffTwoArgJacobianPrep) = prep.config.duals[2]
