c1931c19ddc51e305c14daeb5d5ff9954821c302
Matt Signorelli <mgs255@cornell.edu>
2025-01-23 15:07:36 -0500

    no implicit imports test

--- DifferentiationInterface/test/Back/GTPSA/test.jl
+++ DifferentiationInterface/test/Back/GTPSA/test.jl
@@ -5,6 +5,9 @@ using DifferentiationInterface, DifferentiationInterfaceTest
 using GTPSA: GTPSA
 using Test
 
+using ExplicitImports
+check_no_implicit_imports(DifferentiationInterface)
+
 LOGGING = get(ENV, "CI", "false") == "false"
 
 for backend in [AutoGTPSA()]
