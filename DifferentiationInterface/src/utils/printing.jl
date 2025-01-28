5a36178f1dcec503c1125870297aab4ee654f626
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-01-28 13:02:03 +0100

    fix: better warning for MethodError

--- DifferentiationInterface/src/utils/printing.jl
+++ DifferentiationInterface/src/utils/printing.jl
@@ -1,24 +1,32 @@
-package_name(b::AbstractADType) = package_name(typeof(b))
+required_packages(b::AbstractADType) = required_packages(typeof(b))
 
-function package_name(::Type{B}) where {B<:AbstractADType}
+function required_packages(::Type{B}) where {B<:AbstractADType}
     s = string(B)
     s = chopprefix(s, "ADTypes.")
     s = chopprefix(s, "Auto")
     k = findfirst('{', s)
     if isnothing(k)
-        return s
+        return [s]
     else
-        return s[begin:(k - 1)]
+        return [s[begin:(k - 1)]]
     end
 end
 
-function package_name(::Type{SecondOrder{O,I}}) where {O,I}
-    p1 = package_name(O)
-    p2 = package_name(I)
-    return p1 == p2 ? p1 : "$p1, $p2"
+function required_packages(::Type{SecondOrder{O,I}}) where {O,I}
+    p1 = required_packages(O)
+    p2 = required_packages(I)
+    return unique(vcat(p1, p2))
 end
 
-package_name(::Type{<:AutoSparse{D}}) where {D} = package_name(D)
+function required_packages(::Type{MixedMode{F,R}}) where {F,R}
+    p1 = required_packages(F)
+    p2 = required_packages(R)
+    return unique(vcat(p1, p2))
+end
+
+function required_packages(::Type{<:AutoSparse{D}}) where {D}
+    return unique(vcat(required_packages(D), "SparseMatrixColorings"))
+end
 
 function document_preparation(operator_name::AbstractString; same_point=false)
     if same_point
