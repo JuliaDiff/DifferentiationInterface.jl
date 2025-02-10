1a95c7cfab8fa8b57a8067f6abd1de1a2baa385a
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-02-10 15:40:09 +0100

    test: non-regression for Mooncake deepcopy (#726)

--- DifferentiationInterface/test/Back/Mooncake/test.jl
+++ DifferentiationInterface/test/Back/Mooncake/test.jl
@@ -23,3 +23,11 @@ test_differentiation(
     excluded=SECOND_ORDER,
     logging=LOGGING,
 );
+
+@testset "NamedTuples" begin
+    ps = (; A=rand(5), B=rand(5))
+    myfun(ps) = sum(ps.A .* ps.B)
+    grad = gradient(myfun, backends[1], ps)
+    @test grad.A == ps.B
+    @test grad.B == ps.A
+end
