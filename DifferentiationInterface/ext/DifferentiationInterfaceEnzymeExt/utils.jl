d3f5dd0b11f6ae8843f12f1a5410a6153f714aae
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-03-18 13:13:39 +0100

    Enzyme

--- DifferentiationInterface/ext/DifferentiationInterfaceEnzymeExt/utils.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceEnzymeExt/utils.jl
@@ -53,7 +53,9 @@ force_annotation(f::F) where {F} = Const(f)
     return Const(DI.unwrap(c))
 end
 
-@inline function _translate(backend::AutoEnzyme, ::Mode, ::Val{B}, c::DI.Cache) where {B}
+@inline function _translate(
+    backend::AutoEnzyme, ::Mode, ::Val{B}, c::Union{DI.Cache,DI.PrepContext}
+) where {B}
     if B == 1
         return Duplicated(DI.unwrap(c), make_zero(DI.unwrap(c)))
     else
