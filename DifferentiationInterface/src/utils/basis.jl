467c784edd937b101dd8cb5033d89ce3f30401f7
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-04-15 21:34:07 +0200

    perf: optimize multibasis for sparse differentiation

--- DifferentiationInterface/src/utils/basis.jl
+++ DifferentiationInterface/src/utils/basis.jl
@@ -61,12 +61,15 @@ end
     multibasis(a::AbstractArray, inds)
 
 Construct the sum of the `i`-th standard basis arrays in the vector space of `a` for all `i ∈ inds`.
+
+!!! warning
+    Does not work on GPU, since this is intended for sparse autodiff and SparseMatrixColorings.jl doesn't work on GPUs either.
 """
 function multibasis(a::AbstractArray{T}, inds) where {T}
     b = similar(a)
     fill!(b, zero(T))
     for i in inds
-        b .+= OneElement(i, one(T), a)
+        b[i] = one(T)
     end
     return ismutable_array(a) ? b : map(+, zero(a), b)
 end
