52a91fe9188a4e1853463555560133309d50a853
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-05-10 07:10:25 +0200

    Clearer message

--- DifferentiationInterface/ext/DifferentiationInterfaceEnzymeExt/init.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceEnzymeExt/init.jl
@@ -1,3 +1,9 @@
+const HINT_END = "\n\nThis hint appears because DifferentiationInterface and Enzyme are both loaded. It does not necessarily imply that Enzyme is being called through DifferentiationInterface.\n\n"
+
+function HINT_START(option)
+    return "\nIf you are using Enzyme by selecting the `AutoEnzyme` object from ADTypes, you may want to try setting the `$option` option as follows:"
+end
+
 function __init__()
     # robust against internal changes
     condition = (
@@ -14,29 +20,23 @@ function __init__()
             # robust against internal changes
             Base.Experimental.register_error_hint(T) do io, exc
                 if occursin("EnzymeMutabilityException", string(nameof(T)))
+                    printstyled(io, HINT_START("function_annotation"); bold=true)
                     printstyled(
                         io,
-                        "\nIf you are using Enzyme through DifferentiationInterface, you may want to try modifying the ADTypes backend object as follows:";
-                        bold=true,
-                    )
-                    printstyled(
-                        io,
-                        "\n\n\tAutoEnzyme(; function_annotation=Enzyme.Duplicated)\n\n";
+                        "\n\n\tAutoEnzyme(; function_annotation=Enzyme.Duplicated)";
                         color=:cyan,
                         bold=true,
                     )
+                    printstyled(io, HINT_END; italic=true)
                 elseif occursin("EnzymeRuntimeActivityError", string(nameof(T)))
+                    printstyled(io, HINT_START("mode"); bold=true)
                     printstyled(
                         io,
-                        "\nIf you are using Enzyme through DifferentiationInterface, you may want to try modifying the ADTypes backend object as follows:";
-                        bold=true,
-                    )
-                    printstyled(
-                        io,
-                        "\n\n\tAutoEnzyme(; mode=Enzyme.set_runtime_activity(Enzyme.Forward))\n\tAutoEnzyme(; mode=Enzyme.set_runtime_activity(Enzyme.Reverse))\n\n";
+                        "\n\n\tAutoEnzyme(; mode=Enzyme.set_runtime_activity(Enzyme.Forward))\n\tAutoEnzyme(; mode=Enzyme.set_runtime_activity(Enzyme.Reverse))";
                         color=:cyan,
                         bold=true,
                     )
+                    printstyled(io, HINT_END; italic=true)
                 end
             end
         end
