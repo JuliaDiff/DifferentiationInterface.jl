5ce30ea1e5373c4d9c8214c6c09018dffa9d122b
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-05-17 10:58:05 +0200

    Update DifferentiationInterface/src/docstrings.jl

--- DifferentiationInterface/src/docstrings.jl
+++ DifferentiationInterface/src/docstrings.jl
@@ -27,7 +27,7 @@ function docstring_prepare(operator; samepoint=false, inplace=false)
         In some settings, invalid preparations may still give correct results (e.g. for backends that require no preparation), but this is not a semantic guarantee and should not be relied upon.
 
     !!! danger
-        The preparation result `prep` is **not thread-safe**. Sharing it between threads may lead to unexpected behavior. If you need to run differentiations concurrently, prepare separate `prep` objects for each thread.
+        The preparation result `prep` is **not thread-safe**. Sharing it between threads may lead to unexpected behavior. If you need to run differentiation concurrently, prepare separate `prep` objects for each thread.
 
     When `strict=Val(true)` (the default), type checking is enforced between preparation and execution (but size checking is left to the user).
     While your code may work for different types by setting `strict=Val(false)`, this is not guaranteed by the API and can break without warning.
