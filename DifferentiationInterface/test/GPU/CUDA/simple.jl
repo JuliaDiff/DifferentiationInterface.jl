39a68e49121ba5d775ef4c0f431164e9fe09c13b
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-06-10 12:40:18 +0200

    Add CUDA tests

--- DifferentiationInterface/test/GPU/CUDA/simple.jl
+++ DifferentiationInterface/test/GPU/CUDA/simple.jl
@@ -2,4 +2,26 @@
 using Pkg
 Pkg.add("CUDA")
 using CUDA
+using DifferentiationInterface
+import DifferentiationInterface as DI
+using LinearAlgebra
+using Test
+
 CUDA.versioninfo()
+
+@testset "Basis" begin
+    x = CuVector(rand(Float32, 3))
+    b = basis(x, 2)
+    @test Array(b) == [0, 1, 0]
+
+    X = CuMatrix(rand(Float32, 2, 2))
+    B = DI.multibasis(X, [2, 3])
+    @test Array(B) == [0 1; 1 0]
+end
+
+@testset "Jacobian" begin
+    x = CuVector(rand(Float32, 3))
+    backend = DI.AutoSimpleFiniteDiff()
+    J = jacobian(identity, backend, x)
+    @test J == I
+end
