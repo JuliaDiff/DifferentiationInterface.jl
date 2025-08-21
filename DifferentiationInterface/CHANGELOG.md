4a59a3a9ab58180411ffc6bb221090a16bd93555
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-08-21 08:56:05 +0200

    feat: compute `Scenario` results with a reference backend (#839)

--- DifferentiationInterface/CHANGELOG.md
+++ DifferentiationInterface/CHANGELOG.md
@@ -5,7 +5,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased](https://github.com/JuliaDiff/DifferentiationInterface.jl/compare/DifferentiationInterface-v0.7.5...main)
+## [Unreleased](https://github.com/JuliaDiff/DifferentiationInterface.jl/compare/DifferentiationInterface-v0.7.6...main)
+
+## [0.7.6](https://github.com/JuliaDiff/DifferentiationInterface.jl/compare/DifferentiationInterface-v0.7.5...DifferentiationInterface-v0.7.6)
+
+### Fixed
+
+  - Put test deps into `test/Project.toml` ([#840](https://github.com/JuliaDiff/DifferentiationInterface.jl/pull/840))
+  - Set up `pre-commit` ([#837](https://github.com/JuliaDiff/DifferentiationInterface.jl/pull/837))
 
 ### Fixed
 
--- DifferentiationInterfaceTest/test/scenario.jl
+++ DifferentiationInterfaceTest/test/scenario.jl
@@ -1,17 +1,34 @@
 using DifferentiationInterface
 using DifferentiationInterfaceTest
+using DifferentiationInterfaceTest: default_scenarios
 using ForwardDiff: ForwardDiff
 using Test
 
-scen = Scenario{:gradient,:out}(
-    sum, zeros(10); res1=ones(10), name="My pretty little scenario"
-)
-@test string(scen) == "My pretty little scenario"
+@testset "Naming" begin
+    scen = Scenario{:gradient,:out}(
+        sum, zeros(10); res1=ones(10), name="My pretty little scenario"
+    )
+    @test string(scen) == "My pretty little scenario"
 
-testset = test_differentiation(
-    AutoForwardDiff(), [scen]; testset_name="My amazing test set"
-)
+    testset = test_differentiation(
+        AutoForwardDiff(), [scen]; testset_name="My amazing test set"
+    )
 
-data = benchmark_differentiation(
-    AutoForwardDiff(), [scen]; testset_name="My amazing test set"
-)
+    data = benchmark_differentiation(
+        AutoForwardDiff(), [scen]; testset_name="My amazing test set"
+    )
+end;
+
+@testset "Compute results" begin
+    scens = default_scenarios()
+    new_scens = map(s -> compute_results(s, AutoForwardDiff()), scens)
+
+    isapprox_robust(x, y) = isapprox(x, y)
+    isapprox_robust(x::Nothing, y::Nothing) = true
+    isapprox_robust(x::NTuple, y::NTuple) = all(map(isapprox, x, y))
+
+    for (sa, sb) in zip(scens, new_scens)
+        @test isapprox_robust(sa.res1, sb.res1)
+        @test isapprox_robust(sa.res2, sb.res2)
+    end
+end;
