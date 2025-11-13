d811fad557947f687a0621d19498d9e97b6317bc
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-11-13 19:05:48 +0100

    Coverage

--- DifferentiationInterface/ext/DifferentiationInterfaceMooncakeExt/utils.jl
+++ DifferentiationInterface/ext/DifferentiationInterfaceMooncakeExt/utils.jl
@@ -3,4 +3,3 @@ get_config(backend::AnyAutoMooncake{<:Config}) = backend.config
 
 @inline zero_tangent_unwrap(c::DI.Context) = zero_tangent(DI.unwrap(c))
 @inline Dual_unwrap(c, dc) = Dual(DI.unwrap(c), dc)
-@inline CoDual_unwrap(c, dc) = CoDual(DI.unwrap(c), dc)
