0f0f6572216e59711f94223cc6463d20a377197d
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-11-16 09:51:28 +0100

    exp: investigate new allocations on sparse ForwardDiff Jacobian

--- DifferentiationInterface/test/Back/ForwardDiff/benchmark.jl
+++ DifferentiationInterface/test/Back/ForwardDiff/benchmark.jl
@@ -11,6 +11,20 @@ using Test
 
 LOGGING = get(ENV, "CI", "false") == "false"
 
+@info "Printing allocs"
+const f! = DIT.diffsquare!
+x, y = rand(10), zeros(9);
+backend = MyAutoSparse(AutoForwardDiff());
+prep = prepare_jacobian(f!, y, backend, x);
+J = similar(sparsity_pattern(prep), Float64)
+jacobian!(f!, y, J, prep, backend, x)
+
+Profile.Allocs.clear()
+Profile.Allocs.@profile sample_rate = 1 jacobian!(f!, y, J, prep, backend, x)
+Profile.Allocs.print()
+
+@info "Printing allocs done"
+
 @testset verbose = true "Benchmarking static" begin
     filtered_static_scenarios = filter(static_scenarios(; include_batchified = false)) do scen
         DIT.function_place(scen) == :out && DIT.operator_place(scen) == :out
