5b143d9127f2c3a9f328c30fe96f9b39604aa032
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2026-06-02 11:41:24 +0200

    Matrix

--- DifferentiationInterface/test/Back/Enzyme/test.jl
+++ DifferentiationInterface/test/Back/Enzyme/test.jl
@@ -18,7 +18,7 @@ end
 
 f(x::AbstractArray) = sum(vec(x .^ 4) .* transpose(vec(x .^ 6)))
 
-x = [1.0, 2.0]
-dx = [3.0, 4.0]
+x = [1.0;;]
+dx = [3.0;;]
 c = similar(x)
 @test_nowarn hvp(StoreInCache(f), backend, x, (dx,), Cache(x))
