89e47ada218556b40c1c1c1b3475b97f8ad9a8f9
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2026-08-14 12:27:09 +0200

    Fix

--- DifferentiationInterface/test/runtests.jl
+++ DifferentiationInterface/test/runtests.jl
@@ -6,23 +6,19 @@ include("testutils.jl")
 ## Main tests
 
 if haskey(ENV, "JULIA_DI_TEST_GROUP")
-    folders = [
-        ENV["JULIA_DI_TEST_GROUP"],
-    ]
+    folders = [ENV["JULIA_DI_TEST_GROUP"]]
 else
-    folders = [
-        joinpath("Core", "Internals"),
-        joinpath("Core", "SimpleFiniteDiff"),
-        joinpath("Core", "ZeroBackends"),
-    ]
+    folders = ["Internals", "SimpleFiniteDiff", "ZeroBackends"]
 end
 
-@time @testset verbose = true "DifferentiationInterface.jl" begin
+@time @testset verbose = true "DifferentiationInterface.jl (Core)" begin
     @testset verbose = true "$folder" for folder in folders
-        @testset verbose = true "$file" for file in readdir(joinpath(@__DIR__, folder))
+        @testset verbose = true "$file" for file in readdir(
+                joinpath(@__DIR__, "Core", folder)
+            )
             endswith(file, ".jl") || continue
             @info "Testing $folder/$file"
-            include(joinpath(@__DIR__, folder, file))
+            include(joinpath(@__DIR__, "Core", folder, file))
             yield()
         end
     end
